e9aad9c5c8222b01a5c33ecb9b611b14f5c0439f
S M Sakif Shahriyar <shakif1998@gmail.com>
2025-09-18 11:37:20 +0600

    Update SignUpUiState.kt

--- app/src/main/java/com/sakif/facultyattendance/viewmodel/SignUpUiState.kt
+++ app/src/main/java/com/sakif/facultyattendance/viewmodel/SignUpUiState.kt
@@ -2,6 +2,7 @@ package com.sakif.facultyattendance.viewmodel
 
 sealed class SignUpUiState {
     object Idle : SignUpUiState()
+    object Loading : SignUpUiState()
     object Success : SignUpUiState()
     data class Error(val message: String) : SignUpUiState()
 }
