21abac8eb5091f70a4548bddc73d8d1c4b8221bd
S M Sakif Shahriyar <shakif1998@gmail.com>
2025-09-18 15:31:22 +0600

    Create SignInUiState.kt

---
+++ app/src/main/java/com/sakif/facultyattendance/viewmodel/SignInUiState.kt
@@ -0,0 +1,8 @@
+package com.sakif.facultyattendance.ui
+
+sealed class SignInUiState {
+    object Idle : SignInUiState()
+    object Loading : SignInUiState()
+    object Success : SignInUiState()
+    data class Error(val message: String) : SignInUiState()
+}
